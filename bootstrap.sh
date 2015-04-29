11f83e60c882d9b6d87b2f4b65ea3e4ffe568b80
George Stocker <george@jewelbots.com>
2015-04-29 09:07:57 -0400

    adding

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+brew install ant
+brew install npm
+brew install node
+npm install -g grunt-cli
+npm install -g bower
+npm install -g yeoman
+npm install -g ionic-generator
+grunt platform:add:ios
+grunt platform:add:android
+grunt plugin:add:https://github.com/randdusing/BluetoothLE
+grunt plugin:add:org.apache.cordova.dialogs
+
+
