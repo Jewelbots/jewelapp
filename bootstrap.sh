dae6660a1ef0a92fafc559fcb9854beafc5e3184
George Stocker <george@jewelbots.com>
2015-05-04 15:33:04 -0400

    modifying bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,11 @@
 brew install ant
 brew install npm
 brew install node
+
 npm install -g grunt-cli
 npm install -g bower
 npm install -g yeoman
+npm install -g ionic
 npm install -g ionic-generator
 grunt platform:add:ios
 grunt platform:add:android
