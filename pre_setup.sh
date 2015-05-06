1210f3db5ad4f67484e5acbc78d2666014b19fb7
Scott C. Reynolds <scott@scottcreynolds.com>
2015-05-06 13:31:33 -0400

    bootstrap changes and getting things setup

---
+++ pre_setup.sh
@@ -0,0 +1,19 @@
+# run from parent directory, e.g. if project will be ~/code/jewelapp, run this from ~/code
+set -v
+
+brew install ant
+brew install node
+
+npm install -g grunt-cli
+npm install -g bower
+npm install -g yo
+npm install -g ionic
+npm install -g generator-ionic
+npm install -g cordova
+
+ionic start jewelapp blank
+cd jewelapp
+git init
+git remote add origin git@github.com:Jewelbots/jewelapp.git
+git fetch --all
+git reset --hard origin/master
