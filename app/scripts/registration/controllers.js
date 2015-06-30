ac3b8777e7e2b9d36f4f137beb827bf2d5e3adef
George Stocker <george@jewelbots.com>
2015-06-30 12:10:05 -0400

    stopping point.

--- app/scripts/registration/controllers.js
+++ app/scripts/registration/controllers.js
@@ -31,13 +31,13 @@ angular.module('jewelApp.controllers')
         //}
     };
     $scope.getAvailableDevices = function() {
-
+      var services = [];
       $logService.LogMessage('Getting devices');
       var params = {'serviceUuids': []};
       $ionicPlatform.ready(function () {
         params.request = true;
         $logService.LogMessage('Entering initalization');
-        $logService.LogMessage('ble initialized:\n' + JSON.stringify(params));
+
         $cordovaBluetoothle.initialize(params).then(function (result) {
           $logService.LogMessage('result of initialize call: ' + JSON.stringify(result));
           $logService.LogMessage('Stepping into scanning');
@@ -46,37 +46,69 @@ angular.module('jewelApp.controllers')
               $logService.LogMessage('found! : ' + JSON.stringify(data));
               devices.push(data);
             } else {
-              $logService.LogMessage('still scanning');
+              $logService.LogMessage('still scanning' + JSON.stringify(data));
             }
           }, function (err) {
             $logService.LogMessage('error while scanning ' + JSON.stringify(err));
           }, function (notify) {
             $logService.LogMessage('notifying: ' + JSON.stringify(notify));
-          });
+          }).then($cordovaBluetoothle.stopScan().then(function(result) {
+            $logService.LogMessage('stopping scan ' + JSON.stringify(result));
+            $logService.LogMessage('Stepping into characteristic search');
+            var addCharacteristics = function (services, characteristic, service, index) {
+              $scope.services[index] = service[index];
+              $scope.services[index].characteristics.push(characteristic);
+            };
+            var addError = function (error, params) {
+              $logService.LogError(error, 'Failed to get characteristics for services: '+ JSON.stringify(params));
+            };
+
+            $logService.LogMessage('stepping into decision matrix');
+            if ($ionicPlatform.isIOS()) {
+              $logService.LogMessage('is IOS');
+              var params = {
+                address : devices[0].address,
+                serviceUuids : []
+              };
+              $cordovaBluetoothle.services(function(result) {
+                $logService.LogMessage('found result in services scan: ' + JSON.stringify(result));
+                services.push(result);
+              }, function (error) {
+                $logService.LogError(error, 'Failed to get device: services' + JSON.stringify(params));
+              }, params).then(function(result) {
+                for(var i = 0; i < services.length; i = i + 1) {
+                  $cordovaBluetoothle.characteristics(addCharacteristics(services, result, services[i], i), function (error) {addError(error, params); });
+                }
+              });
+            }
+            else if ($ionicPlatform.isAndroid()) {
+              //todo
+            }
+          }));
 
         }, function (alreadyInit) {
           $logService.LogMessage('already initialized: ' + JSON.stringify(alreadyInit));
         }).then(function () {
-            $logService.LogMessage('stopping scan');
-            $timeout($cordovaBluetoothle.stopScan, 10000);
-        }).then(function () {
+          var addCharacteristics = function (services, characteristic, service, index) {
+            $scope.services[index] = service[index];
+            $scope.services[index].characteristics.push(characteristic);
+          };
+          var addError = function (error, params) {
+              $logService.LogError(error, 'Failed to get characteristics for services: '+ JSON.stringify(params));
+          };
+
           if ($ionicPlatform.isIOS()) {
             var params = {
               address : devices[0].address,
               serviceUuids : []
             };
             $cordovaBluetoothle.services(function(result) {
-              $scope.services.push(result);
+              services.push(result);
             }, function (error) {
               $logService.LogError(error, 'Failed to get device: services' + JSON.stringify(params));
-            }, params).then(function() {
-              var params = {};
+            }, params).then(function(result) {
               for(var i = 0; i < services.length; i = i + 1) {
-                $cordovaBluetoothle.characteristics(function (result) {
-                  $scope.services[i].characteristics.push(result);
-                }, function (error) {
-                  $logService.LogError(error, 'Failed to get characteristics for services: '+ JSON.stringify(params));
-                })
+                $cordovaBluetoothle.characteristics(addCharacteristics(services, result, services[i], i), function (error) {addError(error, params); });
               }
             });
           }
