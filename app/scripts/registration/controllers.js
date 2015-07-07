bb8c9eb48f406f2410b6bc8ec6b28f1b6f5e0ace
George Stocker <george@jewelbots.com>
2015-07-07 13:01:06 -0400

    updating for production use of BLE.

--- app/scripts/registration/controllers.js
+++ app/scripts/registration/controllers.js
@@ -5,7 +5,7 @@ angular.module('jewelApp.controllers')
     //};
     $scope.services = [];
     $scope.model = {
-      status : [],
+      status : 'starting...',
       devices : [],
       errors : [],
       messages : []
@@ -29,84 +29,48 @@ angular.module('jewelApp.controllers')
         //  $scope.model.status.push('didn\'t succeed' + paired);
         //}
     };
-    var contains = function (arr, str) {
-      var len = arr.length;
-      var i;
-      for (i = 0; i < len; i = i + 1) {
-        if (arr[i] === str) {
-          return true;
-        }
-      }
-      return false;
-    };
-    $scope.getAvailableDevices = function () {
-      var addresses = [];
 
-        $ionicPlatform.ready()
-          .then(function () {
-            var params = {request: true};
-            return $cordovaBluetoothle.initialize(params)
-              .then(function (initialized) {
-                $logService.LogMessage('Initialized! : ' + JSON.stringify(initialized));
-                return $cordovaBluetoothle.startScan(params);
-              },
-              function (err) {
-                $logService.LogError('not initalized?: ' + JSON.stringify(err));
-                if (err.hasOwnProperty('isInitialized') && !err.isInitialized) {
-                  return $cordovaBluetoothle.initialize()
-                    .then(function (initialized) {
-                      var params = {request: true};
-                      $logService.LogMessage('Initialized! : ' + JSON.stringify(initialized));
-                      return $cordovaBluetoothle.startScan(params);
-                    })
-                    .then(function (data) {
-                      $logService.LogMessage('data is: ' + JSON.stringify(data));
-                      if (data.status === 'scanResult' && !(contains(addresses, data.address))) {
-                        $logService.LogMessage('pushing new data: 2' + JSON.stringify(data));
-                        $scope.model.devices.push(data);
-                        return $cordovaBluetoothle.isScanning();
-                      }
-                      else {
-                        $logService.LogMessage('stopping scan');
-                        return $cordovaBluetoothle.isScanning();
-                      }
-                    }, function (error) {
-                      $logService.LogError(error, 'Error while scanning:');
-                    }, function (notify) {
-                      $logService.LogMessage('still scanning: ' + JSON.stringify(notify));
-                    }).then(function (scanning) {
-                      $logService.LogMessage('result of scanning then is: ' + JSON.stringify(scanning));
-                      if (scanning) {
-                        return $cordovaBluetoothle.stopScan();
-                      }
-                    });
-                }
-                else {
-                  $logService.LogMessage('not sure how we got here: ' + JSON.stringify(err));
-                }
-                $logService.LogMessage('don\'t you love logging?');
-              })
-              .then(function (data) {
-                $logService.LogMessage('raw data from initialize?' + JSON.stringify(data));
-                if (data.status === 'scanResult' && !(contains(addresses, data.address))) {
-                  $logService.LogMessage('pushing new data: 1 ' + JSON.stringify(data));
-                  $scope.devices.push(data);
-                }
-                else {
-                  $logService.LogMessage('stopping scan');
-                  return $cordovaBluetoothle.stopScan();
-                }
-              }, function (error) {
-                $logService.LogError(error, 'Error while scanning:');
-              }, function (notify) {
-                $logService.LogMessage('still scanning: ' + JSON.stringify(notify));
-              });
-          }).then(function (result) {
-            $logService.LogMessage('we\'re done here? ' + JSON.stringify(result));
-          }).then(function (result) {
-            $logService.LogMessage('this happens last.' + JSON.stringify(result));
+    $scope.getAvailableDevices = function () {
+      var params = {
+        request: true
+      };
+      $ionicPlatform.ready()
+        .then(function () {
+          return $cordovaBluetoothle.initialize(params)
+            .then(function (initialized) {
+              $logService.LogMessage('initialized: ' + JSON.stringify(initialized));
+              $scope.model.status = 'Bluetooth Initialized!';
+              return $cordovaBluetoothle.startScan(params);
+            });
+        })
+        .then(function (data) {
+          $scope.model.status = 'Scanning...';
+          if (data.status === 'scanResult') {
+            $scope.model.status = 'Found device: ' + data.name;
+            $logService.LogMessage('pushing new data: 1 ' + JSON.stringify(data));
+            $scope.model.devices.push(data);
+            return $cordovaBluetoothle.stopScan();
+          }
+        }, function (error) {
+          $scope.model.status = 'Error while scanning.' + JSON.stringify(error);
+          return $cordovaBluetoothle.stopScan();
+        }, function (notify) {
+          $logService.LogMessage('still scanning: ' + JSON.stringify(notify));
+        })
+        .then(function () {
+          $scope.model.status = 'ending scan 1';
+          return $cordovaBluetoothle.isScanning().then(function (isScanning) {
+            $logService.LogMessage('Are we scanning? 1' + JSON.stringify(isScanning));
+            $scope.model.status = 'ended scan 1';
           });
-        $logService.LogMessage('this happens first...');
+        })
+        .then(function () {
+          $scope.model.status = 'ending scan 1';
+          return $cordovaBluetoothle.isScanning().then(function(isScanning) {
+            $logService.LogMessage('Are we scanning? 2' + JSON.stringify(isScanning));
+            $scope.model.status = 'ended scan 2';
+          });
+        });
     };
 
     $scope.clearLog = function () {
