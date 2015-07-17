186f1ecf549aaabd0f97d4b10e8e392aba68e387
George Stocker <george@jewelbots.com>
2015-07-17 16:39:14 -0400

    adding contact pick code to test contacts functionality

--- app/scripts/friends/controllers.js
+++ app/scripts/friends/controllers.js
@@ -1,6 +1,14 @@
 'use strict';
 angular.module('jewelApp.controllers')
 
-.controller('FriendsCtrl', ['$scope', function($scope){
-  $scope.consoleLog = 'hi';
+.controller('FriendsCtrl', ['$scope','$logService', '$cordovaContacts', $ionicPlatform, function($scope, $logService, $cordovaContacts, $ionicPlatform){
+  $scope.findFriendsToAdd = function() {
+    $logService.Log('message', 'entering findFriendsToAdd');
+    $ionicPlatform.ready().then(function () {
+      $logService.Log('message', 'entering we are ready in ionicPlatform');
+      $cordovaContacts.pickContact().then (function (contactPicked) {
+        $logService.Log('message', 'contact picked: ' + JSON.stringify(contactPicked));
+      });
+    });
+  };
 }]);
