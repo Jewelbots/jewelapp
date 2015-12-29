e958a3b98859415562b3b57de992d47868566a1b
Emily Rose <nexxy@symphonysubconscious.com>
2015-12-29 12:52:59 -0800

    Added $logService dependency, refactor scope variables, add selectDevice function.

--- app/scripts/demo/demo-ctrl.js
+++ app/scripts/demo/demo-ctrl.js
@@ -4,13 +4,22 @@ angular
 	.controller('DemoCtrl', [
 		'$scope',
 		'$cordovaBluetoothle',
-		function($scope, $cordovaBluetoothle) {
-			$scope.model = {
-				pairedDevices: [ ],
-				detectedDevices: [ ],
-				numDetected: 0,
-			};
-			$scope.devices = [ ];
+		'$logService',
+		function($scope, $cordovaBluetoothle, $logService) {
+			$scope.devices = {
+				detected: [ ],
+				selected: [ ],
+				paired: [ ]
+			}
+			$scope.numDetected = 0;
+			$scope.numSelected = 0;
+
+			$scope.selectDevice = function selectDevice(device) {
+
+				if(isSelected(device)) { return unselectDevice(device); }
+				$scope.devices.selected.push(device);
+				$scope.numSelected = $scope.devices.selected.length;
+			}
 			$scope.scanForDevices = function scanForDevices() {
 
 				// scan for nearby BLE devices here, ones with JWB_ prefix
