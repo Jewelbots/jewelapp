86c2f2c5cc79ac30de6cd573bda625a678089105
George Stocker <gjstocker@gmail.com>
2015-12-31 12:53:28 -0500

    whoops; got nested too deeply.

--- app/scripts/demo/demo-ctrl.js
+++ app/scripts/demo/demo-ctrl.js
@@ -7,12 +7,12 @@ angular
 		'$ionicPlatform',
 		'$logService',
 		function($scope, $cordovaBluetoothle, $ionicPlatform, $logService) {
-
+      var currentlyScanning = false;
       $scope.devices = {
         detected: [],
         selected: []
       };
-      var isSelected = function(device) {
+      var isSelected = function (device) {
         var sel = $scope.devices.selected.filter(function (item) {
           if (device.name === item.name) {
             return true;
@@ -20,65 +20,72 @@ angular
         });
         return !!sel.length;
       };
-      var stopRefresh = function() {
+      var stopRefresh = function () {
         $scope.$broadcast('scroll.refreshComplete');
       };
 
-      $scope.numDetected = function() { return $scope.devices.detected.length; };
-      $scope.numSelected = function() { return $scope.devices.selected.length; };
+      $scope.numDetected = function () {
+        return $scope.devices.detected.length;
+      };
+      $scope.numSelected = function () {
+        return $scope.devices.selected.length;
+      };
 
       var getAvailableDevices = function () {
         var params = {
           request: true,
-          scanDuration : 10000
+          scanDuration: 10000
         };
-        $ionicPlatform.ready( function () {
-            $logService.Log('message', 'entered ionic ready');
-            return $cordovaBluetoothle.initialize(params)
-              .then(function () {
-                $logService.Log('message', 'entered pre-start scan');
-                return $cordovaBluetoothle.isScanning().then(function (isScanning) {
-                  if (!isScanning) {
-                    return $cordovaBluetoothle.startScan(params);
-                  }
-                });
-              }, function (err) {
-                $logService.Log('error', 'Error trying to initialize bluetoothle ' + JSON.stringify(err));
-              })
-              .then(function (data) {
-                var i;
-                $logService.Log('message', 'data was: ' + JSON.stringify(data));
-                if (data.status === 'scanResult') {
-                  for (i = 0; i < data.length; i = i + 1) {
-                    if (data[i].name.slice(0, 3).toLowerCase() === 'jwb' || data[i].name.slice(0, 3).toLowerCase() === 'jew') {
-                      $scope.devices.detected.push(data[i]);
-                      $logService.Log('detected devices are: ' + JSON.stringify($scope.devices.detected));
-                    }
+        $ionicPlatform.ready(function () {
+          $logService.Log('message', 'entered ionic ready');
+          return $cordovaBluetoothle.initialize(params)
+            .then(function () {
+              $logService.Log('message', 'entered pre-start scan');
+              currentlyScanning = true;
+              return $cordovaBluetoothle.startScan(params);
+            }, function (err) {
+              $logService.Log('error', 'Error trying to initialize bluetoothle ' + JSON.stringify(err));
+            })
+            .then(function (data) {
+              var i;
+              $logService.Log('message', 'data was: ' + JSON.stringify(data));
+              if (data.status === 'scanResult') {
+                for (i = 0; i < data.length; i = i + 1) {
+                  if (data[i].name.slice(0, 3).toLowerCase() === 'jwb' || data[i].name.slice(0, 3).toLowerCase() === 'jew') {
+                    $scope.devices.detected.push(data[i]);
+                    $logService.Log('detected devices are: ' + JSON.stringify($scope.devices.detected));
                   }
                 }
                 return $cordovaBluetoothle.stopScan();
-              }, function (error) {
-                $logService.Log('error', 'Error while scanning.' + JSON.stringify(error));
-                return $cordovaBluetoothle.stopScan();
-              }, function (notify) {
-                $logService.Log('message', 'notifying scan: ' + JSON.stringify(notify));
-              })
-              .then(function () {
-            $logService.Log('message', 'ending scan...');
-            return $cordovaBluetoothle.isScanning().then(function(isScanning) {
-              if (isScanning) {
-                return $cordovaBluetoothle.stopScan();
               }
+            }, function (error) {
+              $logService.Log('error', 'Error while scanning.' + JSON.stringify(error));
+              return $cordovaBluetoothle.stopScan();
+            }, function (notify) {
+              $logService.Log('message', 'notifying scan: ' + JSON.stringify(notify));
+            })
+            .then(function () {
+              $logService.Log('message', 'ending scan...');
+              return $cordovaBluetoothle.isScanning().then(function (isScanning) {
+                if (isScanning) {
+                  currentlyScanning = false;
+                  return $cordovaBluetoothle.stopScan();
+                }
+                else {
+                  currentlyScanning = false;
+                }
+              });
             });
-          });
         });
       };
-
+      
 			$scope.scanForDevices = function() {
-			    getAvailableDevices().then(function (success) {
-			      $logService.Log('message', 'got to success for getAvailableDevices: ' + JSON.stringify(success));
+			  if (!currentlyScanning) {
+          getAvailableDevices().then(function (success) {
+            $logService.Log('message', 'got to success for getAvailableDevices: ' + JSON.stringify(success));
             stopRefresh();
-			    });
+          });
+        }
 
 			};
 
@@ -97,6 +104,7 @@ angular
 			$scope.getDeviceColor = function(device) {
 				return isSelected(device) ? 'item-calm' : 'item-light';
 			};
+			getAvailableDevices();
 		}
 	])
 ;
