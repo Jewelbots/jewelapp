d9357957859679ee6df1f23afb73eb594767d5ac
Emily Rose <nexxy@symphonysubconscious.com>
2015-12-31 10:56:20 -0800

    Added ControlCtrl, controller for demo control tab...

---
+++ app/scripts/demo/control-ctrl.js
@@ -0,0 +1,21 @@
+'use strict';
+var fakeBluetooth = false;
+angular
+	.module('jewelApp.controllers')
+	.controller('ControlCtrl', [
+		'$scope',
+		'$cordovaBluetoothle',
+		'$logService',
+		'DeviceService',
+		function($scope, $cordovaBluetoothle, $logService, DeviceService) {
+			$scope.devices = {
+				detected: function() { return DeviceService.devices.detected; },
+				selected: function() { return DeviceService.devices.selected; }
+			}
+			$scope.numDetected = function() { return DeviceService.numDetected; }
+			$scope.numSelected = function() { return DeviceService.numSelected; }
+
+
+		}
+	])
+;
