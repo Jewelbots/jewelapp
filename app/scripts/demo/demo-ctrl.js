7f2e22ab1187a2b4df9a283b46c85667b0cbed82
Emily Rose <nexxy@symphonysubconscious.com>
2016-01-03 05:51:04 -0800

    Demo select now uses DeviceService.

--- app/scripts/demo/demo-ctrl.js
+++ app/scripts/demo/demo-ctrl.js
@@ -26,49 +26,10 @@ angular
         selected: [ ]
       }
 
-      var getAvailableDevices = function () {
-        var params = {
-          request: true,
-          scanDuration: 10000
-        };
-        $ionicPlatform.ready(function () {
-          $logService.Log('message', 'entered ionic ready');
-          return $cordovaBluetoothle.initialize(params)
-            .then(function () {
-              $logService.Log('message', 'entered pre-start scan');
-              currentlyScanning = true;
-              return $cordovaBluetoothle.startScan(params);
-            }, function (err) {
-              $logService.Log('error', 'Error trying to initialize bluetoothle ' + JSON.stringify(err));
-            })
-            .then(function (data) {
-              var i;
-              $logService.Log('message', 'data was: ' + JSON.stringify(data));
-              for (i = 0; i < data.length; i=i+1) {
-                  if (data[i].status === 'scanResult') {
-                    $scope.devices.detected.push(data[i]);
-                    $logService.Log('detected devices are: ' + JSON.stringify($scope.devices.detected));
-                  }
-              }
-            }, function (error) {
-              $logService.Log('error', 'Error while scanning.' + JSON.stringify(error));
-              return $cordovaBluetoothle.stopScan();
-            }, function (notify) {
-              $logService.Log('message', 'notifying scan: ' + JSON.stringify(notify));
-            })
-            .then(function () {
-              $logService.Log('message', 'ending scan...');
-              return $cordovaBluetoothle.isScanning().then(function (isScanning) {
-                if (isScanning) {
-                  currentlyScanning = false;
-                  return $cordovaBluetoothle.stopScan();
-                }
-                else {
-                  currentlyScanning = false;
-                }
-              });
-            });
-        });
+      $scope.scanForDevices = function() {
+        DeviceService.getAvailableDevices()
+          .then(update)
+          .then(stopRefresh);
       };
 
 			$scope.scanForDevices = function() {
