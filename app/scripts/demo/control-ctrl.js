5e95a0fb464b8546b544b66e513144771360de1a
Emily Rose <nexxy@symphonysubconscious.com>
2016-01-03 06:07:25 -0800

    Fix signature, simplify scope.

--- app/scripts/demo/control-ctrl.js
+++ app/scripts/demo/control-ctrl.js
@@ -6,14 +6,16 @@ angular
 		'$cordovaBluetoothle',
 		'$logService',
 		'DeviceService',
-		function($scope, $cordovaBluetoothle, $logService, DeviceService) {
-			$scope.devices = {
-				detected: function() { return DeviceService.devices.detected; },
-				selected: function() { return DeviceService.devices.selected; }
-			};
-			$scope.numDetected = function() { return DeviceService.numDetected; };
-			$scope.numSelected = function() { return DeviceService.numSelected; };
+		function(
+    $scope,
+    $cordovaBluetoothle,
+    $logService,
+    DeviceService) {
 
+      $scope.devices = {
+        detected: [ ],
+        selected: [ ]
+      }
 
 		}
 	])
