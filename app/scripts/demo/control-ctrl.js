362ebdeb15b7f6d5194015b1ce6f392b503e15b6
Emily Rose <nexxy@symphonysubconscious.com>
2016-01-05 03:57:02 -0800

    ONE GLORIOUS COMMIT.

--- app/scripts/demo/control-ctrl.js
+++ app/scripts/demo/control-ctrl.js
@@ -40,42 +40,173 @@ angular
       $scope.characteristicUuid = undefined;
       $scope.devices.selected = DeviceService.devices.selected;
 
-      var connectStack = $scope.devices.selected;
+      function send(data, device) {
+        $logService.Log('send called...');
+        var thisAddress = device.address;
+        var specialData = _encode(data);
+        var serviceUuid = undefined;
 
-      $scope.familiarize = function() {
-        familiarize(connectStack[0]);
-      };
+        var formalAddress = { address: thisAddress };
 
-      function familiarize(device) {
-        $logService.Log('Familiarizing ourselves with', device.address);
+        $logService.Log('formalAddress is as follows:');
+        $logService.Log(formalAddress);
 
-        var address = { address: device.address };
+        // Start here, friendly interpreter!
 
-        return $cordovaBluetoothle.connect(address).then(function(conn) {
+        $logService.Log('Starting promise chain, weeeeee~!1');
+        initializeTheDeviceAt(thisAddress)
+          .then(areWeStillConnected, errorHandler)
+          .then(discoverWhatItCanDo, errorHandler)
+          .then(writeTheDataToItNow, errorHandler)
+          .then(disconnectFromItNow, errorHandler)
+        ;
 
-          if(conn.status === 'connected') {
+        function initializeTheDeviceAt(theAddressSpecified) {
+          $logService.Log(SEPARATOR);
+          $logService.Log('initializeTheDevice called for ' + theAddressSpecified);
 
-            $logService.Log('Device is connected');
-            return $cordovaBluetoothle.discover(address);
+
+          var a = $q.defer();
+          if(previouslyConnectedDevices[theAddressSpecified]) { // reconnecting
+            $cordovaBluetoothle.reconnect(formalAddress).then(function(conn) {
+              if(conn.status === 'connected') {
+                $logService.Log('initializeTheDevice reconnected.');
+                a.resolve(device);
+              }
+              else if(conn.status === 'connecting') {
+                $logService.Log('initializeTheDevice still reconnecting...');
+                a.reject({ error: 'stillConnecting' });
+              }
+            }, function(err) {
+              $logService.Log('initializeTheDevice reconnect error:');
+              $logService.Log(err);
+              a.reject(err);
+            });
           }
-          else {
-            $logService.Log('Device is not connected.');
+          else { // initial connect
+            $cordovaBluetoothle.connect(formalAddress).then(function(conn) {
+              if(conn.status === 'connected') {
+                previouslyConnectedDevices[thisAddress] = true;
+                $logService.Log('initializeTheDevice connected.');
+                a.resolve(device);
+              }
+              else if(conn.status === 'connecting') {
+                $logService.Log('initializeTheDevice still connecting...');
+                a.reject({ error: 'stillConnecting' });
+              }
+            }, function(err) {
+              $logService.Log('initializeTheDevice connect error:');
+              $logService.Log(err);
+              a.reject(err);
+            });
           }
-        }).then(function(results) {
-          if(results.status === 'discovered') {
-            $logService.Log('Device has been discovered:');
-            $logService.Log('Services:');
-            $logService.Log(results.services);
+          return a.promise;
+        }
 
-            $scope.serviceUuids[device.address] = results.services[0].serviceUuid;
-          }
-        }, function(error) {
-          $logService.Log('Discovery FAILURE:');
+        function areWeStillConnected() {
+          var a = $q.defer();
+          $logService.Log(SEPARATOR);
+          $logService.Log('areWeStillConnected called...');
+          $cordovaBluetoothle.isConnected(formalAddress).then(function(res) {
+            $logService.Log('areWeStillConnected succeeded.');
+            if(res.isConnected) {
+              $logService.Log('CONNECTED');
+              a.resolve(device);
+            }
+            else {
+              $logService.Log('NOT CONNECTED');
+              a.reject({ error: 'connectFailed' });
+            }
+          }, function(error) {
+            $logService.Log('areWeStillConnected error:');
+            $logService.Log(error);
+            a.reject(error);
+          });
+
+          return a.promise;
+        }
+        function discoverWhatItCanDo() {
+          $logService.Log(SEPARATOR);
+          $logService.Log('discoverWhatItCanDo called for ' + thisAddress);
+          var a = $q.defer();
+          $cordovaBluetoothle.discover(formalAddress).then(function(discovery) {
+            if(discovery.status !== 'discovered') {
+              $logService.Log('discoverWhatItCanDo discovery failed.');
+              return a.reject({ error: 'notDiscovered' });
+            }
+            $logService.Log('discoverWhatItCanDo discovery succeeded.');
+            serviceUuid = discovery.services[0].serviceUuid;
+            $logService.Log('serviceUuid:', serviceUuid);
+            a.resolve(device);
+          }, function(error) {
+            $logService.Log('discoverWhatItCanDo discovery error:');
+            $logService.Log(error);
+            a.reject(error);
+          });
+          return a.promise;
+        }
+
+        function writeTheDataToItNow() {
+          $logService.Log(SEPARATOR);
+          $logService.Log('writeTheDataToItNow called...');
+          var a = $q.defer();
+          var magicIncantation = {
+            value: specialData,
+            address: thisAddress,
+            serviceUuid: serviceUuid,
+            characteristicUuid: SET_LED
+          };
+          $logService.Log('magicIncantation is as follows:');
+          $logService.Log(magicIncantation);
+          $cordovaBluetoothle.write(magicIncantation).then(function(wrote) {
+            $logService.Log('writeTheDataToItNow write success:');
+            $logService.Log(wrote);
+            a.resolve(device);
+          }, function(error) {
+            $logService.Log('writeTheDataToItNow write failure:');
+            $logService.Log(error);
+            a.reject(error);
+          });
+          return a.promise;
+        }
+
+        function disconnectFromItNow() {
+          $logService.Log(SEPARATOR);
+          $logService.Log('disconnectFromItNow called...');
+          // TODO: need to call $cordovaBluetoothle.close()
+          var a = $q.defer();
+          $cordovaBluetoothle.close(formalAddress).then(function(really) {
+            if(really.status === 'closed') {
+              $logService.Log('disconnectFromItNow really closed.');
+              $logService.Log(really);
+              a.resolve(device);
+            }
+            else {
+              $logService.Log('disconnectFromItNow failed to close.');
+              $logService.Log(really);
+            }
+            previouslyConnectedDevices[thisAddress] = false;
+          }, function(error) {
+            $logService.Log('disconnectFromItNow error:');
+            $logService.Log(error);
+            a.reject(error);
+          });
+          return a.promise;
+        }
+
+        function errorHandler(error) {
+          $logService.Log(SEPARATOR);
+          $logService.Log('error', 'Error handler was called:');
           $logService.Log(error);
+          disconnectFromItNow();
+          // TODO: Logic so as to recover from various issues
+        }
+      }
 
-        }).then(function() {
-          $cordovaBluetoothle.disconnect(address);
-        });
+      function _encode(data) {
+        var bytes = new Uint8Array(1);
+        bytes[0] = data;
+        return $cordovaBluetoothle.bytesToEncodedString(bytes);
       }
 
       $scope.numSelected = function() {
@@ -152,177 +283,6 @@ angular
       $scope.globalMessage = function() { };
       $scope.linkedParty = function() { };
 
-      // TODO: wrap in isConnected/connect/reconnect logic
-      function write(data, device) {
-
-        $logService.Log('Allocating array');
-        var bytes = new Uint8Array(1);
-        var address = device.address;
-        var serviceUuid = $scope.serviceUuids[address];
-        console.log('serviceUuid:');
-        console.log(serviceUuid);
-        bytes[0] = data;
-
-        $cordovaBluetoothle
-          .isConnected({ address: address })
-          .then(connected, disconnected)
-        ;
-
-        function connected(conn) {
-          $logService.Log('isConnected call successful...');
-          $logService.Log(conn);
-          if(conn.isConnected) {
-            $logService.Log('Device is supposedly connected.');
-            return doWrite();
-          }
-          else {
-            $logService.Log('Device is not connected.');
-            $cordovaBluetoothle.reconnect({ address: address })
-              .then(reconSuccess, reconFailure);
-          }
-        }
-        var writeParams = {
-          value: $cordovaBluetoothle.bytesToEncodedString(bytes),
-          serviceUuid: serviceUuid,
-          characteristicUuid: SET_LED,
-          address: address
-        };
-        $logService.Log('Write parameters:');
-        $logService.Log(writeParams);
-        function disconnected(err) {
-          // whoops, connect for the first time maybe?
-          $logService.Log('Call to isConnected failed.');
-
-          if(err.error === 'neverConnected') {
-            $logService.Log('Never connected to device.');
-            $cordovaBluetoothle.connect({ address: address })
-              .then(function(conn) {
-                if(conn.status === 'connected') {
-                  $timeout(function() {
-                    write(data, device);
-                  }, 500);
-                }
-                else {
-                  $logService.Log('Connect had some trouble:');
-                  $logService.Log(conn);
-                }
-              }, function(err) {
-                $logService.Log('Connect error:');
-                $logService.Log(err);
-              });
-          }
-          $logService.Log(err);
-        }
-
-        function reconSuccess(results) {
-          $logService.Log('Reconnect call successful');
-          $logService.Log(results);
-          if(results.status === 'connected') {
-            $logService.Log('Device is connected now.');
-            discover();
-            // doWrite();
-          }
-          else if(results.status === 'connecting') {
-            // TODO: max retries. UI alert when failed.
-            $logService.Log('Device isn\'t yet connected. Trying again in 500ms.');
-            $timeout(function() {
-              write(data, device);
-            }, 500);
-          }
-          else { // disconnected
-            $logService.Log('Device is not connected/connecting...');
-            $logService.Log(results);
-          }
-        }
-        function reconFailure(err) {
-          $logService.Log('Call to reconnect failed.');
-          $logService.Log(err);
-        }
-
-        function reconnect() {
-          $logService.Log('Attempting to reconnect...');
-          $cordovaBluetoothle
-            .reconnect({ address: device.address })
-            .then(writeIfReconnected, reconError)
-          ;
-        }
-        function disconnect() {
-          $logService.Log('Received disconnect call...');
-          return $cordovaBluetoothle.disconnect({ address: address})
-            .then(function(status) {
-              $logService.Log('Disconnect call successful:');
-              $logService.Log(status);
-            }, function(error) {
-              $logService.Log('Couldn\'t disconnect.');
-              $logService.Log(error);
-            }
-          );
-        }
-
-        function discover() {
-          return $cordovaBluetoothle.services({ address: address })
-            .then(function(res) {
-              var charRequest = {
-                address: address,
-                serviceUuid: serviceUuid
-              };
-              return $cordovaBluetoothle.characteristics(charRequest);
-            }, function(err) {
-              $logService.Log('Discovering services failed.');
-              $logService.Log(err);
-            }).then(function(results) {
-              $logService.Log('Discovered characteristics.');
-              $logService.Log(results);
-              doWrite();
-            }, function(error) {
-              $logService.Log('Error with characteristics.');
-              $logService.Log(error);
-            });
-        }
-        function doWrite() {
-          $logService.Log('Writing ' + data + ' to ' + address);
-          return $cordovaBluetoothle.write(writeParams)
-            .then(disconnect, writeError);
-        }
-        function writeError(err) {
-          $logService.Log('Write error:');
-          $logService.Log(err);
-
-          if(err.error === 'service') {
-            // rediscover
-            $logService.Log('Services have not been discovered');
-          }
-          disconnect();
-        }
-        function writeIfReconnected(recon) {
-          if(recon.status === 'connected') {
-            $logService.Log('Device has been reconnected.');
-            doWrite();
-          }
-          else {
-            $logService.Log('Error reconnecting, status:', recon.status);
-          }
-        }
-
-        function reconError(err) {
-          $logService.Log('Error reconnecting:');
-          $logService.Log(err);
-        }
-      }
-
-      function characteristics(dev) { return ble('characteristics', dev); }
-      function isConnected(dev) { return ble('isConnected', dev); }
-      function disconnect(dev) { return ble('disconnect', dev); }
-      function services(dev) { return ble('services', dev); }
-      function connect(dev) { return ble('connect', dev); }
-
-      function ble(method, device) {
-        return $cordovaBluetoothle[method]({ address: device.address });
-      }
-
-      function writable(elm) {
-        return elm.properties && elm.properties.write;
-      }
 		}
 	])
 ;
